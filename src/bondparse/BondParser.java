7aad709c70878064bd05d54b2847c7a5eb51fb94
laohubzbs <20174932+synkathairo@users.noreply.github.com>
2016-10-29 17:54:46 -0400

    Commented Hashmap fixed ScanningIterator

--- src/bondparse/BondParser.java
+++ src/bondparse/BondParser.java
@@ -2,14 +2,32 @@ package bondparse;
 
 import scanIter.ScanningIterator;
 
+//TODO single character for now, later multiple char elements & more complex reactions
 public class BondParser {
+	//	private boolean atSecond;
+	private char oldChar;
+	private charType cT;
+	private charType oldCT;
+	private int atomIndex;
 	private String input;
 	private String[] lBAr;
+	private String[] holdAtoms;
+	private String holdStr;
 	private ScanningIterator sI;
+
+	private enum charType {
+		isUpper, isLower, isNum, isNone
+	}
 	public void setInput(String str) {
 		input = str;
 //		int a = input.length();
 		lBAr = new String[50];
+		holdAtoms = new String[2];
+//		atSecond=false;
+		atomIndex = 0;
+		oldChar = ' ';
+		oldCT = charType.isNone;
+		cT = charType.isNone;
 //		sI = new ScanningIterator();
 	}
 	public String getInput() {
@@ -19,7 +37,25 @@ public class BondParser {
 		sI = new ScanningIterator(input);
 		while (sI.hasNext()) {
 			char ch = sI.next().charAt(0);
+			if (Character.isUpperCase(ch)) {
+				cT = charType.isUpper;
+//				if
+//				if ()
+			} else if (Character.isLowerCase(ch)) {
+				cT = charType.isLower;
 
+			} else if (Character.isDigit(ch)) {
+				cT = charType.isNum;
+			} else {
+				cT = charType.isNone;
+			}
+
+//			if(sI.next().charAt(1)!=null) {
+//
+//			}
+			oldChar = ch;
+			oldCT = cT;
+//			cT = charType.isNone;
 		}
 	}
 	public String[] getBonds() {
--- src/scanIter/ScanningIterator.java
+++ src/scanIter/ScanningIterator.java
@@ -4,81 +4,73 @@ import java.util.Iterator;
 
 public class ScanningIterator implements Iterator<String> {
 
-	private int nowIndex;
-	private int strL;
-	private String str1;
+    private int nowIndex;
+    private int strL;
+    private String str1;
 
-	public ScanningIterator(String str) {
-		str1 = str;
-		strL = this.str1.length();
-		nowIndex = -1;
-	}
+    public ScanningIterator(String str1) {
+        this.str1 = str1;
+        strL = this.str1.length();
+        nowIndex = -1;
+    }
 
-	public void setStr1(String str) {
-		str1 = str;
-		strL = str1.length();
-		nowIndex = -1;
-	}
+    @Override
+    public boolean hasNext() {
+        if (nowIndex < (strL - 1)) {
+            nowIndex += 1;
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	@Override
-	public boolean hasNext() {
-		if(nowIndex<(strL-1)) {
-			nowIndex+=1;
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
+    @Override
+    public String next() {
+        return str1.substring(nowIndex, nowIndex + 1);
+        //nowIndex++;
+//        return currentCharStr;
+    }
 
-	@Override
-	public String next() {
-		String currentCharStr = str1.substring(nowIndex,nowIndex+1);
-		//nowIndex++;
-		return currentCharStr;
-	}
-	
-	public int indexOf(String s, char ch, int fromIndex) {
-		int storeInt = nowIndex;
-		String storeStr = str1;
-		str1 = s;
-		nowIndex = fromIndex-1;
-		while(this.hasNext()) {
-			if(this.next().equals(Character.toString(ch))) {
-				int ansInt = nowIndex+1;
-				//restore initial condition before call of indexOf()
-				nowIndex = storeInt;
-				str1 = storeStr;
-				return ansInt;
-			}
-		}
-		nowIndex = storeInt;
-		return -1;
-		//return 0;
-	}
-	
-	public int indexOfNot(String s, char ch, int fromIndex) {
-		int storeInt = nowIndex;
-		String storeStr = str1;
-		str1 = s;
-		nowIndex=fromIndex-1;
-		while(this.hasNext()) {
-			if(this.next().equals(Character.toString(ch))) {
-				
-			}
-			else {
-				int ansInt = nowIndex+1;
-				nowIndex = storeInt;
-				str1=storeStr;
-				return ansInt;
-			}
-		}
-		nowIndex = storeInt;
-		return -1;
-	}
-	
-	public int getIndex() {
-		return nowIndex+1;
-	}
+    public int indexOf(String s, char ch, int fromIndex) {
+        int storeInt = nowIndex;
+        String storeStr = str1;
+        str1 = s;
+        nowIndex = fromIndex - 1;
+        while (this.hasNext()) {
+            if (this.next().equals(Character.toString(ch))) {
+                int ansInt = nowIndex + 1;
+                //restore initial condition before call of indexOf()
+                nowIndex = storeInt;
+                str1 = storeStr;
+                return ansInt;
+            }
+        }
+        nowIndex = storeInt;
+        return -1;
+        //return 0;
+    }
+
+    public int indexOfNot(String s, char ch, int fromIndex) {
+        int storeInt = nowIndex;
+        String storeStr = str1;
+        str1 = s;
+        nowIndex = fromIndex - 1;
+        while (this.hasNext()) {
+            if (!(this.next().equals(Character.toString(ch)))) {
+
+//            } else {
+                int ansInt = nowIndex + 1;
+                nowIndex = storeInt;
+                str1 = storeStr;
+                return ansInt;
+            }
+        }
+        nowIndex = storeInt;
+        return -1;
+    }
+
+    public int getIndex() {
+        return nowIndex + 1;
+    }
 
 }
