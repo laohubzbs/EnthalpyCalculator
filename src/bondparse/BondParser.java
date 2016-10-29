74d6b80abd861e97de634984380b285c4e8228b1
bazaboi <bazaboi@bazaboi-ThinkPad-P50s>
2016-10-29 12:49:49 -0400

    package declaration added

---
+++ src/bondparse/BondParser.java
@@ -0,0 +1,5 @@
+package bondparse;
+
+public class BondParser {
+
+}
--- src/main/Thermo.java
+++ src/main/Thermo.java
@@ -1,3 +1,5 @@
+package main;
+//random comment number one
 public class Thermo {
 	public static void main(String args[]) {
 		System.out.println("Hi");
