a755095343efe1042aeec97de26b769483b12e1c
laohubzbs <20174932+synkathairo@users.noreply.github.com>
2016-10-29 15:34:21 -0400

    testing

---
+++ src/Combust/Combuster.java
@@ -0,0 +1,10 @@
+package Combust;
+
+/**
+ * Created by bazaboi on 10/29/16.
+ */
+public class Combuster {
+    public static void main(String[] args) {
+        System.out.println("FIRE!!!");
+    }
+}
